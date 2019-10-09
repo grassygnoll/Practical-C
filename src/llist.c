875694373ce8c7d0f086d4f078de4f2b0c5508c9
Benjamin Wheeless <benjamin.wheeless@gmail.com>
2019-10-09 15:45:12 -0400

    One node but getting seg fault

--- src/llist.c
+++ src/llist.c
@@ -18,13 +18,34 @@ struct ElementType
 
 struct ListNode 
 {
-    struct ElementType S ;
+    State StateData ;
     NextState Next ;
 } ;
 
 int IsEmpty(StateList L)
 {
     return L->Next == NULL ;
+}
+
+int IsLast(NextState N, StateList L)
+{
+    return N->Next == NULL ;
+}
+
+void Insert(State S, StateList L, NextState N) 
+{
+    NextState tmpState ;
+
+    tmpState = malloc(sizeof(struct ListNode)) ;
+    if(tmpState == NULL)
+    {
+        printf("Fatal Error: Unable to allocate new list node in memory!\n") ;
+        exit(EXIT_FAILURE) ;
+    }
+
+    tmpState->StateData = S ;
+    tmpState->Next = N->Next ;
+    N->Next = tmpState ;
 }
 
 /* Main routine to test our functions */
@@ -42,8 +63,30 @@ int main(void)
     States = malloc(sizeof(struct ListNode)) ;
     States->Next = NULL ;
 
-    if(IsEmpty(States))
-        printf("Our new list is now empty...") ;
+    if(IsEmpty(States)) {
+        printf("Our new list is empty, adding our first node...\n") ;
+        Insert(MyState, States, States) ;
+    } else {
+        printf("Our list is not empty!\n") ;
+    }
+
+    if(IsEmpty(States)) {
+        printf("We have a problem! Just added a node, but list still tests as empty!\n") ;
+    } else {
+        NextState Current ;
+        Current = States->Next ;
+        printf("Current state:      %s\n", Current->StateData->StateName) ;
+        printf("Current state code: %s\n", Current->StateData->StateCode) ;
+
+        /* The below is causing a segmentation fault */
+        /*
+        while(! (IsLast(Current->Next, States))) {
+            printf("Current state:      %s\n", Current->StateData->StateName) ;
+            printf("Current state code: %s\n", Current->StateData->StateCode) ;
+            Current = Current->Next ;
+        }
+        */
+    }
 
     return 0 ;
 }
