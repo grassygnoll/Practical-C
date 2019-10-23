a6dc43ac6813c3a934dfa300216cf48e82f0db75
Benjamin Wheeless <benjamin.wheeless@gmail.com>
2019-10-23 15:44:17 -0400

    Added inorder() function, started on deltree() function

--- src/btree-mit.c
+++ src/btree-mit.c
@@ -76,6 +76,38 @@ void preorder(leafptr root)
 }
 
 /*******************************************************************************
+ * inorder() - accepts ptr to root of tree and displays the leaves using       *
+ *             in-order traversal (left side, then root first, then right).    *
+ ******************************************************************************/
+void inorder(leafptr root)
+{
+    if(root != NULL)
+    {
+        inorder(root->left) ;
+        printf("[%p] data=%d\tleft->%p\tright->%p\n", root, root->data, 
+            root->left, root->right) ;
+        inorder(root->right) ;
+    }
+}
+
+/*******************************************************************************
+ * deltree() - accepts ptr to root of tree and deletes all leaves on the tree  *
+ *             using post-order traversal (left side, right side, then root).  *
+ *           - returns count (int) of leaves deleted.                          *
+ ******************************************************************************/
+int deltree(leafptr root)
+{
+    int count = 0 ;
+
+    if(root != NULL)
+    {
+        count += deltree(root->left) ;
+        count += deltree(root->right) ;
+        /* don't think this works yet, need to also add in the leaf delete/free logic */
+    }
+}
+
+/*******************************************************************************
  * main() - Main program entry point with test driving code                    *
  ******************************************************************************/
 int main(void)
@@ -87,7 +119,11 @@ int main(void)
     for(i = 0; i < MAX_INTS; i++)
         trunk = add_leaf(trunk, Numbers[i]) ;
 
+    printf("Binary tree contents in \"preorder\" display:\n") ;
     preorder(trunk) ;
+
+    printf("\nBinary tree contents in \"inorder\" display:\n") ;
+    inorder(trunk) ;
 
     return 0 ;
 }
